397e8b9913caab47e1b725ebe7a1f2749251eb91
Akshat Panchal <132990890+akshatpanchal18@users.noreply.github.com>
2026-09-03 19:06:29 +0530

    feat: update authentication types and extend Express request interface with Mailbox

--- src/feature/auth/auth.types.ts
+++ src/feature/auth/auth.types.ts
@@ -5,10 +5,3 @@ import { Mailbox, Session, User } from "../../generated/prisma/client";
 export type CreateUserInput = z.infer<typeof createUserSchema>;
 
 export type LoginUserInput = z.infer<typeof loginUserSchema>;
-export type UserAuthPayload = { type: "u"; userId: string; sessionId: string };
-export type GuestAuthPayload = {
-  type: "g";
-  mailboxId: string;
-  sessionId: string;
-};
-export type AuthPayload = UserAuthPayload | GuestAuthPayload;
--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,11 +1,11 @@
-import { Guest, Session, User } from "../generated/prisma/client";
+import { Guest, Mailbox, Session, User } from "../generated/prisma/client";
 
 declare global {
   namespace Express {
     interface Request {
       user?: User;
       session?: Session;
-      guest?: Guest;
+      mailbox?: Mailbox;
     }
   }
 }
