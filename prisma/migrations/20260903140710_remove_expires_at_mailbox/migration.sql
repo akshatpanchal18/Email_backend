cb0b2f8cd3eef0ad3c3a0ee893d96028e3c5636d
Akshat Panchal <132990890+akshatpanchal18@users.noreply.github.com>
2026-09-03 20:03:20 +0530

    feat: remove expiresAt column from Mailbox model and migration

---
+++ prisma/migrations/20260903140710_remove_expires_at_mailbox/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `expiresAt` on the `Mailbox` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Mailbox" DROP COLUMN "expiresAt";
