61e870f2c3bc362cf8078a7650b579288edab126
Akshat Panchal <132990890+akshatpanchal18@users.noreply.github.com>
2026-09-05 18:19:19 +0530

    fix: update cookie options to set SameSite attribute to 'none'

--- src/feature/auth/auth.controller.ts
+++ src/feature/auth/auth.controller.ts
@@ -7,7 +7,7 @@ class AuthController {
   private static readonly cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax" as const,
+    sameSite: "none" as const,
     path: "/",
     maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
   };
